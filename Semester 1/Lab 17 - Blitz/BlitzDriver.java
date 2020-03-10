e526eba6667a190ace26d3bf6869d2e988a2fc3b
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-09 19:54:01 -0700

    Create BlitzDriver.java

---
+++ Semester 1/Lab 17 - Blitz/BlitzDriver.java
@@ -0,0 +1,10 @@
+//Hornung
+//Lab 17 - Blitz
+// To demonstrate a pivotal method regarding arrays (shuffling), as well as to familiarize with and
+//                                                   demonstrate our current understanding of arrays.
+public class BlitzDriver {
+    public static void main( String[] args ) {
+        Blitz blitz = new Blitz(); //creates a Blitz game with 2 players
+        blitz.play(); //starts game play
+    }
+}

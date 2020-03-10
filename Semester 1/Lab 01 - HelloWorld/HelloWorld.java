2826de0e2ebad3370a692dc6c0b03bb17df4bc43
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-09 19:25:33 -0700

    Uploaded lab from BlueJ
    😀╰(*°▽°*)╯

--- Semester 1/Lab 01 - HelloWorld/HelloWorld.java
+++ Semester 1/Lab 01 - HelloWorld/HelloWorld.java
@@ -1,10 +1,8 @@
 //Ivan
 //Lab 1 - HelloWorld
 //Become familiar with Java and BlueJ
-public class HelloWorld
-{
-    public static void main( String[] args )
-    {
+public class HelloWorld {
+    public static void main( String[] args ) {
         System.out.println( "Hello, World!");
         System.out.println( "This is Ivan Hornung :)");
         System.out.println( "I've been looking forward to this class since freshman year!");

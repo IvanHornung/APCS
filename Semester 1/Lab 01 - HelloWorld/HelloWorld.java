a375a6d3f56504b3f9e2e4085f3627c0298a4b64
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-09 19:23:59 -0700

    🍕

---
+++ Semester 1/Lab 01 - HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+//Ivan
+//Lab 1 - HelloWorld
+//Become familiar with Java and BlueJ
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, World!");
+        System.out.println( "This is Ivan Hornung :)");
+        System.out.println( "I've been looking forward to this class since freshman year!");
+        System.out.println( "I share the same birthday as my "+  ( 11 % 4 ) + " year old sister!");
+        System.out.println("Let's do this!");
+    }
+}

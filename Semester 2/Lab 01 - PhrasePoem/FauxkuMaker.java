fe04de11a37e0625bb7a16ccbaeffafdc6a3cb7f
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-10 18:36:56 -0700

    Create FauxkuMaker.java

---
+++ Semester 2/Lab 01 - PhrasePoem/FauxkuMaker.java
@@ -0,0 +1,65 @@
+public class FauxkuMaker
+{
+    private Word[] words;
+
+    //initialize and fill the array words with 30 or more Word objects of your choice
+    //Hint: to create a Word object, you must provide both the word and the number of syllables
+    public FauxkuMaker()
+    {
+        words = new Word[52];
+
+        words[0] = new Word("Jahseh", 2);
+        words[1] = new Word("bestowed", 2);
+        words[2] = new Word("crusty", 2);
+        words[3] = new Word("vibe", 1);
+        words[4] = new Word("maggot", 2);
+        words[5] = new Word("propaganda", 4);
+        words[6] = new Word("Albania", 4);
+        words[7] = new Word("crawlspace", 2);
+        words[8] = new Word("cheese", 1);
+        words[9] = new Word("arousal", 3);
+        words[10] = new Word("belly button lint", 5);
+        words[11] = new Word("quantum", 2);
+        words[12] = new Word("warmth", 1);
+        words[13] = new Word("violence", 3);
+        words[14] = new Word("homeless shelter", 4);
+        words[15] = new Word("cocoon", 2);
+        words[16] = new Word("Fortnite", 2);
+        words[17] = new Word("dampens", 2);
+        words[18] = new Word("car insurance", 4);
+        words[19] = new Word("tapeworm", 2);
+        words[20] = new Word("Dequavis", 3);
+        words[21] = new Word("worships", 2);
+        words[22] = new Word("ensues", 2);
+        words[23] = new Word("incubates", 3);
+        words[24] = new Word("captures", 2);
+        words[25] = new Word("intrigues", 2);
+        words[26] = new Word("wrestles", 2);
+        words[27] = new Word("conforms", 2);
+        words[28] = new Word("secretes", 2);
+        words[29] = new Word("oozes", 2);
+        words[30] = new Word("releases", 3);
+        words[31] = new Word("slithers", 2);
+        words[32] = new Word("consumes", 2);
+        words[33] = new Word("deploys", 2);
+        words[34] = new Word("compresses", 3);
+        words[35] = new Word("scams", 1);
+        words[36] = new Word("stimulates", 3);
+        words[37] = new Word("summons", 2);
+        words[38] = new Word("dampens", 2);
+        words[39] = new Word("moistens", 2);
+        words[40] = new Word("husbands", 2);
+        words[41] = new Word("purchases", 3);
+        words[42] = new Word("dominates", 3);
+        words[43] = new Word("operates", 3);
+        words[44] = new Word("Obama", 3);
+        words[45] = new Word("rubs", 1);
+        words[46] = new Word("sacrifices", 4);
+        words[47] = new Word("liposuction", 4);
+        words[48] = new Word("decays", 2);
+        words[49] = new Word("rots", 1);
+        words[50] = new Word("Cambodian", 4);
+        words[51] = new Word("Yemen", 2);
+
+    }
+

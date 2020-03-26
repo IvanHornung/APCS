347ba409dc98bbfff6a6732cc7434b73577b0706
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-25 22:02:30 -0700

    Made values final wherever possible

--- Semester 2/Lab 10 - Eight Queens/EightQueens.java
+++ Semester 2/Lab 10 - Eight Queens/EightQueens.java
@@ -26,7 +26,7 @@ public class EightQueens {
 
     //Pre-Condition: method is called with y,x parameters of the newly formed queen's coordinates
     //Post-Condition: T/F is return depending on whether the coordinates are not in another queen's range
-    public static boolean isSafe(int r, int c) {
+    public static boolean isSafe(final int r, final int c) {
         for(int i = 0; i < rd.length; i++ ) {
             int m = 1; //multiplier
             while((r+rd[i]*m < board.length && r+rd[i]*m >=0) && (c+cd[i]*m < board[r+rd[i]*m].length && c+cd[i]*m >= 0)){
@@ -39,7 +39,7 @@ public class EightQueens {
   
     //Pre-Condition: method is called with parameter 0
     //Post-Condition: recursive method adjusts the board to place queens in a manner where eight queens can fit
-    public static boolean hasEightQueens(int row) {
+    public static boolean hasEightQueens(final int row) {
         if(row == board.length) return true;
         for( int c = 0; c < board[row].length; c++ ) {
             if(isSafe(row,c)) {
